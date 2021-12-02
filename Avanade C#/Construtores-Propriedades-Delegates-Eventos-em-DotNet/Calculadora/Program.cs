42856dddd7d6ead21e8e81b4f1ca0cf822d0eaa9
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2021-12-02 20:15:22 -0300

    atualização
    finalizando modulo com eventos e delegates

--- Avanade C#/Construtores-Propriedades-Delegates-Eventos-em-DotNet/Calculadora/Program.cs
+++ Avanade C#/Construtores-Propriedades-Delegates-Eventos-em-DotNet/Calculadora/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Calculadora
+﻿using Calculadora.Models;
+
+namespace Calculadora
 {
     class Program
     {
+        public delegate void Operacao(int x, int y);
         static void Main(string[] args)
         {
-            
+            Matematica m = new Matematica(10, 20);
+            m.Somar();
+
+            // Operacao op = new Operacao(Calculadora.Somar);
+            // op += Calculadora.Subtrair;
+
+            // op.Invoke(10, 10);
         }
     }
 }
