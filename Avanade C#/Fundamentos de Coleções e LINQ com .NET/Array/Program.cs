001b1de383e669736e354631e98b49617bf8b21c
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2021-12-02 22:06:38 -0300

    atualização
    iniciando arrays

---
+++ Avanade C#/Fundamentos de Coleções e LINQ com .NET/Array/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matriz = new int[4,2] 
+            {
+                { 8, 8 },
+                { 10, 20 },
+                { 50, 100 },
+                { 90, 200 }
+            };
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    System.Console.WriteLine(matriz[i, j]);
+                }
+            }
+            // int[] arrayInteiros = new int[3];
+
+
+            // arrayInteiros[0] = 10;
+            // arrayInteiros[1] = 20;
+            // arrayInteiros[2] = 30;
+
+
+
+            // foreach(int i in arrayInteiros)
+            // {
+            //     System.Console.WriteLine(i);
+            // }
+        }
+    }
+}
