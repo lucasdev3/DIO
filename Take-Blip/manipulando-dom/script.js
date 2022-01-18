4d476d04397463fdb3283f47db9c66bd4ca15e0e
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2022-01-18 15:51:21 -0300

    jogo da nave

--- Take-Blip/manipulando-dom/script.js
+++ Take-Blip/manipulando-dom/script.js
@@ -1,3 +1,10 @@
+const frase = 'lifes not a problem to be soved.';
+const n = 14;
 
-const [precoAlcool, precoGasolina, rendimentoAlcool, rendimentoGasolina] = lines[0].split(' ').map(x => parseFloat(x));
-console.log(precoAlcool);
+if (n>=1 && n<=34) {
+
+  const fraseParcial = texto.substr(0, n)
+  print(fraseParcial.toUpperCase());
+  console.log(fraseParcial.toUpperCase());
+
+}
